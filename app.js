f3813712cb813f08bea8477aa2de6964e323a021
quangtv <quangtv@rabiloo.com>
2022-09-12 10:06:23 +0700

    update project config and structure

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
+const app = express()
 const port = process.env.PORT || 3001
+const corsOptions = {
+    origin: '*', //allow cors origin for all client
+}
 
-express().get('/', (req, res) => {
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// use cors
+app.use(cors(corsOptions))
+
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-express().listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
---
+++ src/configs/mysql.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: process.env.MYSQL_PORT,
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    dialect: process.env.MYSQL_DIALECT,
+  }
+  
